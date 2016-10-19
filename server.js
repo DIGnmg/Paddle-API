d23ad5459749ce53245584fa7a35d5f9a4534e7d
Nathanael Gethers <gethernm@gmail.com>
2016-10-19 10:00:40 -0500

    change

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const Hapi = require('hapi');
 const Bcrypt = require('bcrypt-nodejs');
 const Basic = require('hapi-auth-basic');
+const CookieAuth = require('hapi-auth-cookie');
 
 //Database
 const db = require('./adapters/db.js');
@@ -30,6 +31,39 @@ const validate = function (request, username, password, callback) {
     });
 };
 
+const  cookieValidate = function (request, session, callback) {
+
+    cache.get(session.sid, (err, cached) => {
+
+        if (err) {
+            return callback(err, false);
+        }
+
+        if (!cached) {
+            return callback(null, false);
+        }
+
+        return callback(null, true, cached.account);
+});
+
+let cookieAuth = function (server, options, next) {
+    const cache = server.cache({ segment: 'sessions', expiresIn: 3 * 24 * 60 * 60 * 1000 });
+    server.app.cache = cache;
+    
+    server.auth.strategy('base', 'cookie', {
+      password: 'supersecretpassword', // cookie secret
+      cookie: 'app-cookie', // Cookie name
+      ttl: 24 * 60 * 60 * 1000, // Set session to 1 day
+      validateFunc: cookieValidate
+    });
+    return next();
+};
+
+cookieAuth.attributes = {
+    name: 'auth-cookie',
+    version: '1.0.0'
+};
+
 let simpleAuth = function (server, options, next) {
     server.auth.strategy('simple', 'basic', { validateFunc: validate });
     return next();
@@ -40,7 +74,7 @@ simpleAuth.attributes = {
     version: '1.0.0'
 };
 
-const plugins = [Basic, simpleAuth, Players, Matches, GameTypes, Leagues];
+const plugins = [CookieAuth, cookieAuth, Players, Matches, GameTypes, Leagues];
 server.register(plugins, (err) => {
   
   if (err) {
